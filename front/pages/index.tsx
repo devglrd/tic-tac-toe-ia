f53f4ef474f2e0daa4ace2a980a4fcb4743f9991
remi guillard <remi@moostack.fr>
2022-08-24 15:16:33 +0200

    Front minimax

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -2,7 +2,7 @@ import type {NextPage} from 'next'
 import {useEffect, useState} from "react";
 import Cell from "../components/cell";
 import axios from 'axios';
-import {act} from "react-dom/test-utils";
+import {act, Simulate} from "react-dom/test-utils";
 import it from "node:test";
 
 const Home: NextPage = () => {
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
     const [iaScore, setIaScore] = useState([] as any[]);
     const [magicHuman, setMagicHuman] = useState([] as any[]);
     const [magicIa, setMagicIa] = useState([] as any[]);
+
 
     async function clickOnCell(index: number) {
         if (winner) return;
@@ -27,6 +28,10 @@ const Home: NextPage = () => {
         await setIsX(false)
         if (await isWinning(magicHumanScoreTemp)) {
             setWinner('X');
+            return false;
+        }
+        if (!canPlay()) {
+            setWinner('draw');
             return false;
         }
         await playBot(humanScoreTemp)
@@ -45,13 +50,29 @@ const Home: NextPage = () => {
         return false
     }
 
+    async function getWinnerScore() {
+        if (await isWinning(magicHuman)) {
+            return -1;
+        }
+        if (await isWinning(magicIa)) {
+            return 1;
+        }
+        if (!canPlay()) {
+            return 0;
+        }
+        return null;
+
+
+    }
+
     async function playBot(score: any) {
 
-        const play = await axios.post('https://tic-tac-toe-ia-api.vercel.app/api/play', {
-            humanScore: score,
-            iaScore
-        });
-        const index = play.data;
+        // const play = await axios.post('https://tic-tac-toe-ia-api.vercel.app/api/play', {
+        //     humanScore: score,
+        //     iaScore
+        // });
+        // const index = play.data;
+        const index = await findBestMove();
         const iaScoreTemp = [...iaScore, index]
         cell[index] = '0';
         const magicIaTemp = [...magicIa, magicBoard[index]]
@@ -63,6 +84,62 @@ const Home: NextPage = () => {
             setWinner('0');
             return false;
         }
+
+        if (!canPlay()) {
+            setWinner('draw');
+            return false;
+        }
+    }
+
+    async function findBestMove() {
+        let bestScore = -100;
+        let bestMove = 0;
+        for (let i = 0; i < 9; i++) {
+            if (cell[i] !== null) continue;
+            cell[i] = '0';
+            const score = await minimax(cell, false);
+            cell[i] = null;
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = i;
+            }
+        }
+
+        return bestMove;
+    }
+
+    async function minimax(copyBoard: any, isMazimizing: boolean) {
+        const winner = await getWinnerScore();
+        if (winner !== null) {
+            return winner
+        }
+        let bestScore = isMazimizing ? -100 : 100;
+        for (let i = 0; i < 9; i++) {
+            if (copyBoard[i] === null) {
+                copyBoard[i] = isMazimizing ? '0' : 'X';
+
+                if (isMazimizing) {
+                    setMagicIa([...magicIa, magicBoard[i]])
+                } else {
+                    setMagicHuman([...magicHuman, magicBoard[i]])
+                }
+                const score = await minimax(copyBoard, !isMazimizing);
+
+                copyBoard[i] = null;
+                if (isMazimizing) {
+                    setMagicIa(magicIa.pop())
+                } else {
+                    setMagicHuman(magicHuman.pop())
+                }
+                bestScore = isMazimizing ? Math.max(score, bestScore) : Math.min(score, bestScore);
+            }
+        }
+
+        return bestScore;
+    }
+
+    function canPlay() {
+        return cell.filter(cell => cell === null).length > 0;
     }
 
     async function reset() {
